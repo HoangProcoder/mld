1e994fdf5a01c9d82207365aa2ace97eda9452ec
HoangProcoder <89496247+HoangProcoder@users.noreply.github.com>
2023-09-10 23:32:11 +0700

    Update autodown.js

--- autodown.js
+++ autodown.js
@@ -130,7 +130,7 @@ function isFileSizeValid(filepath) {
 
 async function downloadYouTube(url, api, event, itag = 249) {
   try {
-    var data = await ytdl.getInfo(link)
+    var data = await ytdl.getInfo(url)
         var result = {
             title: data.videoDetails.title,
             dur: Number(data.videoDetails.lengthSeconds),
